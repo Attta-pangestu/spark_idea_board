83af2f017e8c94fcf0efae72d8565cd2b2068d88
Atha <athabarokahfc@gmail.com>
2024-05-02 14:30:05 +0700

    setup tailwind config

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "./components/ui/Button";
+const page = () => {
+  return (
+    <div>
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default page;
