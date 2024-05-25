242250e8e23aee5d4e7ce75b0a25681c0c9a4a7f
Atha <athabarokahfc@gmail.com>
2024-05-25 22:18:05 +0700

    process: render rect element on canvas

--- app/board/[boardId]/_components/LayerPreview.tsx
+++ app/board/[boardId]/_components/LayerPreview.tsx
@@ -3,11 +3,12 @@
 import { useStorage } from "@/liveblocks.config";
 import React, { memo } from "react";
 import { ILayerEnum, ILayerType } from "@/types/canvas";
+import { RectangleShape } from "./LayerType/Rectangle";
 
 interface ILayerPreview {
   id: string;
   selectionColor: string;
-  onPressedDown: (e: React.PointerEvent) => void;
+  onPressedDown: (e: React.PointerEvent, id: string) => void;
 }
 
 export const LayerPreview = memo(
@@ -17,8 +18,16 @@ export const LayerPreview = memo(
     if (!layer) return null;
 
     switch (layer.typeLayer) {
-      case ILayerEnum.Text:
-        return <div>Rectangle</div>;
+      case ILayerEnum.Rectangle:
+        return (
+          <RectangleShape
+            key={id}
+            id={id}
+            layerProps={layer}
+            onPointerDown={onPressedDown}
+            selectionColor={selectionColor}
+          />
+        );
       default:
         console.log("unknown layer type");
         return null;
--- app/board/[boardId]/_components/LayerType/Rectangle.tsx
+++ app/board/[boardId]/_components/LayerType/Rectangle.tsx
@@ -1,13 +1,34 @@
-interface IRectangleLayer {
+import { ILayerType, IRectangleLayer } from "@/types/canvas";
+
+interface IRectangleShape {
   id: string;
   layerProps: IRectangleLayer;
-  onPointerDown: (e: React.PointerEvent) => void;
+  onPointerDown: (e: React.PointerEvent, id: string) => void;
+  selectionColor?: string;
 }
 
-export const RectangleLayer = () => {
+export const RectangleShape = ({
+  id,
+  layerProps,
+  onPointerDown,
+  selectionColor,
+}: IRectangleShape) => {
+  const { x, y, width, height, fill } = layerProps;
+
+  console.log({});
+
   return (
-    <div>
-      <h1>Rectangle</h1>
-    </div>
+    <rect
+      className="drop-shadow-md"
+      // style={{transform: `translate(${x}px, ${y}px)`}}
+      x={x}
+      y={y}
+      width={width}
+      height={height}
+      strokeWidth={1}
+      fill="#000"
+      stroke={"transparent"}
+      onPointerDown={(e) => onPointerDown(e, id)}
+    />
   );
 };
